912e8c7a33850f37091b6f66d610da44ab06e093
Y Lin <linyeah2011@gmail.com>
2021-10-02 16:08:26 +0800

    m2 l1 functional interface

---
+++ Module2/lesson1/src/Main.java
@@ -0,0 +1,22 @@
+import _06_functional_Interface.BinaryOperation;
+
+public class Main {
+    public static void main(String[] args) {
+        BinaryOperation add = (a, b) -> a + b;
+
+        // Or you could use:
+        //
+        //  BinaryOperation add = Integer::sum;
+        //
+        // More on method references later!
+
+        // Or use Java's built-in functional interface
+        //
+        // import java.util.function.BinaryOperator
+        // BinaryOperator<Integer> add = (a, b) -> a + b;
+        //
+        // it does the same thing!
+
+        assert 5 == add.apply(2, 3);
+    }
+}
---
+++ Module2/lesson1/src/_06_functional_Interface/BinaryOperation.java
@@ -0,0 +1,6 @@
+package _06_functional_Interface;
+
+@FunctionalInterface
+public interface BinaryOperation {
+    int apply(int a, int b);
+}
